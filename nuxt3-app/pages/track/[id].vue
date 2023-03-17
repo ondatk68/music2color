9a171dcebcd90f20bbd6eaa0f3a4c48cc06583e5
恩田健太郎 <ondakentaro0805@g.ecc.u-tokyo.ac.jp>
2023-03-18 00:05:04 +0900

    csvから曲の情報とってきて曲名検索

---
+++ nuxt3-app/pages/track/[id].vue
@@ -0,0 +1,30 @@
+<template>
+    <div style="margin: 50px">
+      {{ rowData }}
+    </div>
+</template>
+
+  <script>
+
+  import data from '@/assets/top20_japan.csv';
+
+  export default{
+    data: ()=>({
+      csvData: [],
+    }),
+    mounted: function() {
+      //console.log(data);
+      data.forEach((value,index) => {
+        this.csvData = data;
+      });
+    },
+    computed: {
+      rowData() {
+        // idに対応するデータを返す
+
+        return this.csvData.find((row, index) => index == this.$route.params.id);
+
+      },
+    }
+  }
+</script>
--- nuxt3-app/pages/usingtitle.vue
+++ nuxt3-app/pages/usingtitle.vue
@@ -1,3 +1,38 @@
 <template>
-    <p>usingtitle</p>
-</template>
+    <div id="app">
+      <div class="search">
+        <input type="text" v-model="keyword" placeholder="searching track..."/>
+
+      </div>
+      <tr v-for="tracks in filteredTracks" :key="tracks.id">
+        <nuxt-link :to="`/track/${tracks.id}`">{{ tracks.name }}</nuxt-link>
+      </tr>
+    </div>
+  </template>
+  <script>
+    import data from '@/assets/top20_japan.csv';
+
+    export default{
+      data: ()=>({
+        keyword: "",
+        tracks: []
+      }),
+      mounted: function() {
+        data.forEach((value,index) => {
+          this.tracks.push({name: value["Track Name"]+" - "+value["Artist Name"], lower: value["Track Name"]+" - "+value["Artist Name"].toLowerCase(), id:index});
+        });
+      },
+      computed: {
+        filteredTracks: function() {
+          var tracks = [];
+          for(var i in this.tracks) {
+            var song  = this.tracks[i];
+            if(song.lower.indexOf(this.keyword.toLowerCase()) !== -1) {
+              tracks.push(song);
+            }
+          }
+          return tracks;
+        }
+      },
+    }
+  </script>
