ce2c4acfbf010f039462a28eb96acdb084f6155f
my-01 <94096241+my-01@users.noreply.github.com>
2023-03-22 11:56:08 +0900

    Update usingtitle.vue

--- nuxt3-app/pages/usingtitle.vue
+++ nuxt3-app/pages/usingtitle.vue
@@ -1,38 +1,82 @@
 <template>
-    <div id="app">
-      <div class="search">
-        <input type="text" v-model="keyword" placeholder="searching track..."/>
+  <div id="app">
+    <div class="container">
+    <div class="search">
+      <input type="text" v-model="keyword" placeholder="searching track..." />
+    </div>
 
-      </div>
-      <tr v-for="tracks in filteredTracks" :key="tracks.id">
-        <nuxt-link :to="`/track/${tracks.id}`">{{ tracks.name }}</nuxt-link>
+      <tr v-for="track in filteredTracks" :key="track.id">
+        <nuxt-link :to="`/track/${track.id}`" class="songlist">{{ track.name }}</nuxt-link>
       </tr>
     </div>
-  </template>
-  <script>
-    import data from '@/assets/song_features9.csv';
+  </div>
+</template>
 
-    export default{
-      data: ()=>({
-        keyword: "",
-        tracks: []
-      }),
-      mounted: function() {
-        data.forEach((value,index) => {
-          this.tracks.push({name: value["Track Name"]+" - "+value["Artist"], lower: value["Track Name"]+" - "+value["Artist"].toLowerCase(), id:index});
-        });
-      },
-      computed: {
-        filteredTracks: function() {
-          var tracks = [];
-          for(var i in this.tracks) {
-            var song  = this.tracks[i];
-            if(song.lower.indexOf(this.keyword.toLowerCase()) !== -1) {
-              tracks.push(song);
-            }
-          }
-          return tracks;
-        }
-      },
+<script>
+import data from '@/assets/top_chart_50.csv';
+
+export default {
+  data() {
+    return {
+      keyword: '',
+      tracks: []
     }
-  </script>
+  },
+  mounted() {
+    data.forEach((value, index) => {
+      this.tracks.push({ name: value["Track Name"] + " - " + value["Artist"], lower: value["Track Name"] + " - " + value["Artist"].toLowerCase(), id: index });
+    });
+  },
+  computed: {
+    filteredTracks() {
+      var tracks = [];
+      for (var i in this.tracks) {
+        var song = this.tracks[i];
+        if (song.lower.indexOf(this.keyword.toLowerCase()) !== -1) {
+          tracks.push(song);
+        }
+      }
+      return tracks;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.search input[type="text"] {
+  width: 300px;
+  height: 20px;
+  font-size: 18px;
+
+  padding: 15px;
+  padding-left: 30px;
+  border: none;
+  border-radius: 40px;
+  background-color: white;
+  outline: none;
+  margin-top: 100px;
+  margin-bottom: 50px;
+  border: 1px solid black;
+}
+
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+}
+
+.songlist {
+  display: flex; /* フレックスボックスとして表示するための設定 */
+  justify-content: space-between; /* アイテムを左右に均等に配置する */
+  align-items: center; /* アイテムを垂直方向中央に配置する */
+  background-color: #f7f7f7;
+  margin: 10px;
+  padding: 50px;
+  width: 500px;
+  color: black; /*テキストの色変化と下線を消したい */
+  text-decoration: none; 
+}
+
+</style>
