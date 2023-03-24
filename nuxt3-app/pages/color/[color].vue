2c1434a90d4c182bb16c26237072dd6bf551bb0c
恩田健太郎 <ondakentaro0805@g.ecc.u-tokyo.ac.jp>
2023-03-24 13:01:08 +0900

    色検索少し

---
+++ nuxt3-app/pages/color/[color].vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="usingcolor" :style="{'background-color': $route.params.color}">
+        <div class="centered">
+            <h1>{{ $route.params.color }}に合う曲はこれ！</h1>
+            <div class="songlist">
+                <NuxtLink to="https://open.spotify.com/track/4F0m8AWIgR7SK6GyvPsPpc?si=e0922c12e4be4a91">
+                    春の歌 スピッツ
+                </NuxtLink>
+            </div>
+            <div class="songlist">
+                <NuxtLink to="https://open.spotify.com/track/1rr2DJOxV0sHXeUXCAz1yf?si=932cb2bcf1a74d5b">
+                    春泥棒 ヨルシカ
+                </NuxtLink>
+            </div>
+            <div class="songlist">
+                <NuxtLink to="https://open.spotify.com/track/3UF51Hc8M8ZFH7nzbZxobZ?autoplay=true">
+                    サクラ咲ケ 嵐
+                </NuxtLink>
+            </div>
+
+        </div>
+    </div>
+    <a @click="backButton" class = "btn">戻る</a>
+</template>
+
+
+<style>
+.btn {
+  border-radius: 30px;
+  background-color: rgb(250, 85, 85);
+  padding: 20px;
+  text-decoration: none;
+  color: rgb(0, 0, 0);
+}
+</style>
+
+<script>
+export default {
+  methods: {
+    backButton(){
+      this.$router.go(-1);
+    }
+  },
+}
+</script>
--- nuxt3-app/pages/usingcolor.vue
+++ nuxt3-app/pages/usingcolor.vue
@@ -1,45 +1,72 @@
 <template>
-     <div class="usingcolor">
-        <div class="centered">
-            <h1>この色に合う曲はこれ！</h1>
-            <div class="songlist">
-                <NuxtLink to="https://open.spotify.com/track/4F0m8AWIgR7SK6GyvPsPpc?si=e0922c12e4be4a91">
-                    春の歌 スピッツ
-                </NuxtLink>
-            </div>
-            <div class="songlist">
-                <NuxtLink to="https://open.spotify.com/track/1rr2DJOxV0sHXeUXCAz1yf?si=932cb2bcf1a74d5b">
-                    春泥棒 ヨルシカ
-                </NuxtLink>
-            </div>
-            <div class="songlist">
-                <NuxtLink to="https://open.spotify.com/track/3UF51Hc8M8ZFH7nzbZxobZ?autoplay=true">
-                    サクラ咲ケ 嵐
-                </NuxtLink>
-            </div>
-            
+    <div class="question">
+      What color song are you looking for ?
+    </div>
+    <div class="button-container">
+        <div v-for="(color, index) in colors" :key="index">
+        <button class="color-button" :style="{'background-color': color}" @click="goToColorPage(color)">{{ color }}</button>
         </div>
     </div>
-    <a @click="backButton" class = "btn">戻る</a>
 </template>
-
-<style>
-.btn {
-  border-radius: 30px;
-  background-color: rgb(250, 85, 85);
-  padding: 20px;
-  text-decoration: none;
-  color: rgb(0, 0, 0);
-}
-</style>
 
 <script>
 export default {
-  methods: {
-    backButton(){
-      this.$router.go(-1);
+  data() {
+    return {
+      colors: ['Red', 'Orange', 'Yellow', 'Green', 'Lime', 'Blue', 'Aqua', 'Purple', 'Pink', 'Brown', 'Black', 'White']
     }
   },
+  methods: {
+    goToColorPage(color) {
+      this.$router.push(`/color/${color.toLowerCase()}`)
+    }
+  }
 }
 </script>
 
+<style>
+.button-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  flex-wrap: wrap;
+}
+
+.color-button {
+  margin: 1rem;
+  padding: 1.5rem;
+  border-radius: 50%;
+  font-size: 1.2rem;
+  color: #fff;
+  text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  transition: transform 0.2s ease-in-out;
+  position: relative;
+  z-index: 1;
+  overflow: hidden;
+  width: 120px;
+  height: 120px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.color-button:hover {
+  transform: translateY(-10px);
+}
+
+.color-button:active {
+  transform: translateY(0px);
+}
+
+.question{
+    padding:20px;
+    text-align: center;
+    font-family: 'Helvetica Neue', sans-serif;
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+
+</style>
